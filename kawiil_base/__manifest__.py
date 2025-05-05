960501b6e9aff775d9def40c9f8ae282cf6cfbef
Roberto Saavedra <robertsvx27@gmail.com>
2025-05-05 14:23:41 -0400

    update branch 16.0

--- kawiil_base/__manifest__.py
+++ kawiil_base/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Kawiil/Kawiil',
     'author': 'Odoo, Inc.',
     'website': 'https://github.com/odoo-trainings/kawiil-base/',
-    'version': '18.0.1.0.0',
+    'version': '16.0.1.0.0',
     'depends': ['sale_management', 'website', 'mrp', 'contacts'],
     'data': [
         'data/product_data.xml',
